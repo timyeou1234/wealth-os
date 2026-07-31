4afb91f00e44382665d8b9cd20bd2f3628574055
Timothy Yeou <timyeou1234@hotmail.com>
2026-07-31 15:55:05 +0800

    fix(api): enable Flyway auto-configuration

--- apps/api/build.gradle.kts
+++ apps/api/build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.flywaydb:flyway-core")
+    implementation("org.springframework.boot:spring-boot-starter-flyway")
     implementation("org.flywaydb:flyway-database-postgresql")
     runtimeOnly("org.postgresql:postgresql")
 
